8820017d36b733c6fd49e879755f4abbd600f1ad
cryptocreeper94 <jason@livfitoken.com>
2026-05-26 02:10:03 -0500

    fix: Add .js extension to db import in depot-routes for ESM compatibility

--- server/depot-routes.ts
+++ server/depot-routes.ts
@@ -6,7 +6,7 @@
  */
 
 import { Router, Request, Response } from "express";
-import { db } from "./db";
+import { db } from "./db.js";
 import { sql } from "drizzle-orm";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
